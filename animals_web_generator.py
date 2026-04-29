13d929e4a4084120d85b27556774aff5793ad537
kirbysfurby <kirbysfurby@gmail.com>
2026-04-29 12:43:20 +0200

    bessere Struktur

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,40 +7,44 @@ def load_data(file_path):
         return json.load(handle)
 
 
-animals_data = load_data("animals_data.json")
-
-output = ""
-
-for animal in animals_data:
-    output += '<li class="cards__item">\n'
-
-    # Titel
+def serialize_animal(animal):
+    """Converts a single animal object into HTML"""
+    output = '<li class="cards__item">\n'
     output += f'  <div class="card__title">{animal["name"]}</div>\n'
-
-    # Textblock
     output += '  <p class="card__text">\n'
 
     characteristics = animal.get("characteristics", {})
 
     if "diet" in characteristics:
-        output += f'      <strong>Diet:</strong> {characteristics["diet"]}<br/>\n'
+        output += f'    <strong>Diet:</strong> {characteristics["diet"]}<br/>\n'
 
     if "locations" in animal and len(animal["locations"]) > 0:
-        output += f'      <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
+        output += f'    <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
 
     if "type" in characteristics:
-        output += f'      <strong>Type:</strong> {characteristics["type"]}<br/>\n'
+        output += f'    <strong>Type:</strong> {characteristics["type"]}<br/>\n'
 
     output += '  </p>\n'
     output += '</li>\n'
 
-
-with open("animals_template.html", "r", encoding="utf-8") as handle:
-    html_template = handle.read()
+    return output
 
 
-new_html = html_template.replace("__REPLACE_ANIMALS_INFO__", output)
+def main():
+    animals_data = load_data("animals_data.json")
+
+    output = ""
+    for animal in animals_data:
+        output += serialize_animal(animal)
+
+    with open("animals_template.html", "r", encoding="utf-8") as file:
+        template = file.read()
+
+    final_html = template.replace("__REPLACE_ANIMALS_INFO__", output)
+
+    with open("animals.html", "w", encoding="utf-8") as file:
+        file.write(final_html)
 
 
-with open("animals.html", "w", encoding="utf-8") as handle:
-    handle.write(new_html)
+if __name__ == "__main__":
+    main()
